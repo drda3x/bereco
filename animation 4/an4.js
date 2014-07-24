76c595447c31882da18e5be4e0ce8d25876e5dec
da3x <da3x11@gmail.com>
2014-07-25 00:53:05 +0400

    Остались popup'ы

--- animation 4/an4.js
+++ animation 4/an4.js
@@ -5,14 +5,34 @@
 (function (global, google, document) {
     "use strict";
 
+    // Function for getting initialize map
     function initMap() {
         var mapOptions = {
                 center: new google.maps.LatLng(-34.61106492834361,-58.4498405456543),
                 zoom: 12,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             },
-            map = new google.maps.Map(document.getElementById('map'), mapOptions)
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+        map.data.loadGeoJson('/bereco/animation 4/casos.json');
+        map.data.setStyle(function(feature){
+            var prop = feature.getProperty('riesgoTotal'),
+                icon;
+
+            if(prop == 0) {
+                icon = 'icon-white'
+            } else if(prop < 5 ) {
+                icon = 'icon-yellow'
+            } else if(prop < 8) {
+                icon = 'icon-orange'
+            } else {
+                icon = 'icon-red'
+            }
+
+            return {
+                icon: '/bereco/animation 4/'+ icon +'.png'
+            }
+        });
     }
 
     if(!global.animationApi) {
