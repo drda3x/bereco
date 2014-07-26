a6ec8d771bc217f3592c0552df90bd4aef49a4d1
drda3x <da3x11@gmail.com>
2014-07-27 02:11:05 +0400

    Добавлены попапы

--- animation 4/an4.js
+++ animation 4/an4.js
@@ -3,10 +3,41 @@
  */
 
 (function (global, google, document) {
-    "use strict";
+    'use strict';
+
+    function getPopupHtml(content) {
+        var bodyStr = '';
+
+        for(var i= 0, j= content.body.length; i<j; i++) {
+            bodyStr += '<div>'+ content.body[i].name +': '+ content.body[i].value +'</div>';
+        }
+
+        return '<div id="popup"><div>'+ content.header +'</div>'+ bodyStr +'</div>';
+    }
+
+    function getPopupContent(feature) {
+        var type = popupTypes[feature.getProperty('tipo')],
+            contentStr = '';
+
+        for(var i= 0, j=type.length; i<j; i++) {
+            contentStr += '<div>'+ type[i] +': '+ feature.getProperty(type[i]) +'</div>'
+        }
+
+        return contentStr;
+    }
+
+    var popup = new google.maps.InfoWindow({
+        content: 'test'
+        }),
+        popupTypes = {
+            persona: ['riesgoGeografico','edad', 'salud', 'movilidad','riesgoPropio','riesgoTotal'],
+            casa: ['riesgoGeografico', 'plantaBaja','riesgoPropio','riesgoTotal'],
+            auto: ['riesgoGeografico', 'bajoTierra','riesgoPropio','riesgoTotal']
+        };
 
     // Function for getting initialize map
     function initMap() {
+
         var mapOptions = {
                 center: new google.maps.LatLng(-34.61106492834361,-58.4498405456543),
                 zoom: 12,
@@ -33,6 +64,14 @@
                 icon: getUrl(icon) +'.png'
             }
         });
+
+        map.data.addListener('click', function(event) {
+            var feature = event.feature;
+            popup.setPosition(feature.getGeometry().get());
+            popup.setContent(getPopupContent(feature));
+            popup.open(map);
+        });
+
     }
 
     function getUrl(fileName) {
