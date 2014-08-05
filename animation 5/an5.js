358707511635540edb5f5ad3230928926291c594
Nesterov <da3x11@gmail.com>
2014-08-05 16:53:19 +0400

    Начата работа с 5й анимацией

---
+++ animation 5/an5.js
@@ -0,0 +1,156 @@
+/**
+ * <<Описание файла>>
+ */
+
+(function (global, google, document, InfoBox, navigator) {
+    'use strict';
+
+    function Feature() {
+        this.data = null;
+    }
+
+    Feature.prototype.getPopupContent = function() {
+        var self = this,
+            mainProp = self.data.getProperty('tipo'),
+            type = popupTypes[mainProp],
+            contentStr = '<table id="popupContent" cellpadding="0" cellspacing="0">';
+
+        for(var i= 0, j=type.length; i<j; i++) {
+            contentStr += '<tr><td>'+ popupTypeInfo[type[i]] +'</td><td class="values">'+ (function(data) {
+                if (data == 'edad' || data == 'tipo') {
+                    return self.data.getProperty(data);
+                } else {
+                    return self.getScaledParam('wrd', data);
+                }
+            })(type[i])  +'</td></tr>'
+        }
+
+        contentStr += '</table>';
+
+        return '<div id="popup"><div class="header"> Riesgo '+ this.getScaledParam('wrd', 'riesgoTotal') +'</div>'+ contentStr + '</div>' +
+               '<div id="popupArrow"></div>' ;
+    };
+
+    Feature.prototype.getScaledParam = function(type, param) {
+        var params = {
+                height: {
+                    img: 'img/icon-red',
+                    wrd: 'alto'
+                },
+                p_middle: {
+                    img: 'img/icon-orange',
+                    wrd: 'medio alto'
+                },
+                m_middle: {
+                    img: 'img/icon-yellow',
+                    wrd: 'mediano'
+                },
+                lower: {
+                    img: 'img/icon-white',
+                    wrd: 'bajo'
+                }
+            },
+            prop = this.data.getProperty(param);
+        if(param == 'plantaBaja' || param == 'bajoTierra') {
+            if(prop == null) {
+                return 'Si';
+            } else {
+                return 'No';
+            }
+        } else {
+            if(prop == 0) {
+                return params.lower[type];
+            } else if(prop < 5 ) {
+                return params.m_middle[type];
+            } else if(prop < 8) {
+                return params.p_middle[type];
+            } else if(prop > 8){
+                return params.height[type];
+            } else {
+                return null;
+            }
+        }
+    };
+
+    Feature.prototype.getLatLng = function() {
+        return this.data.getGeometry().get();
+    };
+
+    var f = new Feature(),
+        popup = new InfoBox(),
+        popupTypes = {
+            persona: ['tipo', 'riesgoGeografico', 'edad', 'salud', 'movilidad', 'riesgoPropio', 'riesgoTotal'],
+            casa: ['tipo','riesgoGeografico', 'plantaBaja', 'riesgoPropio', 'riesgoTotal'],
+            auto: ['tipo','riesgoGeografico', 'bajoTierra', 'riesgoPropio', 'riesgoTotal']
+        },
+        popupTypeInfo = {
+            tipo: 'Tipo',
+            riesgoGeografico: 'Riesgo geografico',
+            edad: 'Edad',
+            salud: 'Salud',
+            movilidad: 'Movilidad',
+            riesgoPropio: 'Riesgo propio',
+            riesgoTotal: 'Riesgo total',
+            plantaBaja: 'Planta baja',
+            bajoTierra: 'Bajo tierra'
+        };
+
+    // Function for getting initialize map
+    function initMap() {
+
+        var mapOptions = {
+                center: new google.maps.LatLng(-34.61106492834361,-58.4498405456543),
+                zoom: 12,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        map.data.loadGeoJson(getUrl('initial_data/minimas.json'));
+        /*map.data.setStyle(function(feature){
+            f.data = feature;
+            return {
+                icon: getUrl(f.getScaledParam('img', 'riesgoTotal')) +'.png'
+            }
+        });
+
+        map.data.addListener('click', function(event) {
+            f.data = event.feature;
+            popup.setPosition(f.getLatLng());
+            popup.setContent(f.getPopupContent());
+            popup.setOptions({
+                pixelOffset: (function(f){
+                    var n = f.getProperty('tipo');
+                    if(n == 'persona') {
+                        return new google.maps.Size(-105, -260);
+                    } else {
+                        return new google.maps.Size(-105, -205);
+                    }
+                })(f.data)
+            });
+            popup.open(map);
+        });
+        getUserLocation(map);*/
+    }
+
+    function getUserLocation(map) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var lat = position.coords.latitude,
+                lon = position.coords.longitude,
+                marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(lat, lon)
+                })
+        });
+    }
+
+    function getUrl(fileName) {
+        return global.location.pathname.replace(/\w*.\w*$/,'') + fileName
+    }
+
+    if(!global.animationApi) {
+        global.animationApi = {}
+    }
+
+    global.animationApi.initializeMap = initMap;
+
+})(this, google, document, InfoBox, navigator);
