4611617b9d1f4126712e4a2fb7e2cc5c84ed2517
drda3x <da3x11@gmail.com>
2014-09-15 23:55:28 +0400

    Остались пиктограмки

--- weather/js/weatherWidget.js
+++ weather/js/weatherWidget.js
@@ -2,13 +2,13 @@
  * Created by al on 15.09.14.
  */
 
-(function(g) {
+(function (g) {
 
     function initWidget() {
 
-        $.getJSON('../weather/fixture/test.json', function(json) {
+        $.getJSON('../weather/fixture/test.json', function (json) {
 
-           var mapData = (function() {
+            var mapData = (function () {
                 var data = [
                         json.conditions.today,
                         json.conditions.tomorrow,
@@ -17,25 +17,30 @@
                     ],
                     index = 0;
 
-                return function() {
-                    $(this).find('.date').text(data[index].date.replace(/-/g,'/'));
+                return function () {
+                    $(this).find('.date').text(data[index].date.replace(/-/g, '/'));
 
                     var temp = $(this).find('.temperature'),
                         wind = $(this).find('.footer span:first'),
                         humidty = $(this).find('.footer span:last');
 
-                    if($(this).hasClass('current')) {
-                        temp[0].text('MAX ' + data[index].max + '&deg;');
-                        temp[1].text('MIN ' + data[index].min + '&deg;');
+                    if ($(this).hasClass('current')) {
+                        $(this).find('.currentTemperature').html(data[index].temperature_c + '&deg;');
+                        $(temp[0]).html('MAX ' + data[index].max + '&deg;');
+                        $(temp[1]).html('MIN ' + data[index].min + '&deg;');
                     } else {
-                        temp.text(data[index].max + '&deg;/' + data[index].min + '&deg;');
+                        $(temp).html(data[index].max + '&deg;/' + data[index].min + '&deg;');
                     }
 
-                    wind.text(parseFloat(data[index].wind.match(/[0-9]/g)));
-                    humidty.text(data[index].humidty_perc);
+                    wind.html(parseFloat(data[index].wind.match(/\d*\.\d*/g)[0]));
+                    humidty.html(data[index].humidty_perc);
+                    index++;
 
                 }
             })();
+
+            console.log(mapData);
+
             // todo Я что-то забыл, потом проверить все подстановки
             $('.weatherInfoScreen_infoBox_item').each(mapData);
         });
